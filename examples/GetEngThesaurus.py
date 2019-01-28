0f8d3d4cdd27caed8ae25966a90e3d24c4b1128e
SEDict <huangliangzhang@sina.com>
2019-01-28 20:59:15 +0800

    111

--- examples/GetEngThesaurus.py
+++ examples/GetEngThesaurus.py
@@ -2,20 +2,19 @@
 test math
 """
 from DST.domain_thesaurus.DomainThesaurus import DomainThesaurus
-from DST.datasets.CleanData import cleanEngXml
+from DST.datasets.CleanData import cleanEngXml, CleanDataWiki
 import os
 import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 if __name__ == "__main__":
-    #
     # cleanEngXml(xmlPath="D:\\Codes\\PythonProj\\EngDict\\data\\Posts.xml", savePath="E:/testEng/clean_eng.txt")
     # cwiki = CleanDataWiki(wiki_data_path="D:\\Codes\\PythonProj\\SEDict\\data\\enwiki-latest-pages-articles.xml",
     #                       clean_data_path="E:/testMath/clean_wiki.txt")
     # cwiki.tranform()
-    dst = DomainThesaurus(domain_specific_corpus_path="E:/testEng/clean_eng.txt",
-                          general_corpus_path="",
-                          outputDir="E:/testEng",
+    dst = DomainThesaurus(domain_specific_corpus_path="your domain specific corpus path",
+                          general_corpus_path="your general corpus path",
+                          outputDir="path of output",
                           filePaths=None,
                           phrase_detection_domain="default",
                           phrase_detection_general="default",
@@ -23,6 +22,5 @@ if __name__ == "__main__":
                           semantic_related_words="default",
                           word_classification="default",
                           synonym_group="default")
-    # dst.extract()
-    print(dst.filePaths)
-    # print(os.path.join("E:/testEng","Phrases/2_domain"+str(2)+"_phrase.model"))
+    # extract domain thesauruss
+    dst.extract()
